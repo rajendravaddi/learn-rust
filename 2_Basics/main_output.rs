59d8080f3920b2aa94210b9444ba88da580ab2b5
Rajendra <rajendravaddi753@gmail.com>
2026-08-25 14:42:22 +0530

    output formatting

--- 2_Basics/main_output.rs
+++ 2_Basics/main_output.rs
@@ -6,6 +6,9 @@
 
 // main() function - is the entry point of the program
 fn main() {
+
+  // Printing is handled by a series of macros defined in std::fmt
+
   // println! macro - is used to print something to the console
   // ! - indicates that it is a macro
   println!("Hello World!");
@@ -21,5 +24,16 @@ fn main() {
   println!("Hello"); // Places newline character at the end
   print!("World!"); // Does not place newline character at the end
   print!("\n"); // Newline character
+
+  // some examples
+  println!("{one} {two} {three}", two="Hi", one="Hello", three="World");
+  let num =15;
+  println!("Base10 : {}", num);
+  println!("Base2 : {:b}", num);
+  println!("Base8 : {:o}", num);
+  println!("Base16 : {:x}", num);
+
+  println!("{number:->5}", number=num);
+  println!("{number:0>5}", number=num);
   
 }
