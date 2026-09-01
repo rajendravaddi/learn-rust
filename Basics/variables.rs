be06229f486c5d9ec92c77399f383a34646f476c
Rajendra <rajendravaddi753@gmail.com>
2026-09-01 19:42:20 +0530

    Modified variables

--- Basics/variables.rs
+++ Basics/variables.rs
@@ -26,13 +26,61 @@ fn main() {
     shadowing is a feature in Rust that allows you to redeclare a variable with the same name, and the new variable will shadow the previous one. 
     */
     let x = 10;
-    let x = x + 1;
+
+    {
+        let x = x + 1;
+        println!("x = {}",x); // Here x = 11 // inner block can access outer block
+
+    }
     let x = x * 2;
-    println!("x = {}",x);
+    println!("x = {}",x); // here x = 20 // outer block cannot access inner block
 
     /*
     Placeholder - {
     while printing variables using print macros we need to use placeholder {}
     println!("Hello {}", x);
     */
+
+    /*
+    Declare first
+    It is possible to declare variable bindings first and initialize them later, but all variable bindings must be initialized before they are used: the compiler forbids use of uninitialized variable bindings, as it would lead to undefined behavior.
+    */
+    let variable1; // declaration
+    
+    {
+        variable1 = 10;
+    }
+    
+    println!("Variable1 : {}", variable1);
+
+    /*
+
+    let variable2; // declaration
+    println!("Variable2 : {}", variable2);
+
+    It will cause error, because we haven't initialized variable 2, compiler cannot assign a type to it.
+
+    */
+
+    /* 
+    Freezing
+    
+    When data is bound by the same name immutably, it also freezes. Frozen data can’t be modified until the immutable binding goes out of scope:
+    */
+    let mut _mutable_integer = 7i32;
+
+    {
+        // Shadowing by immutable `_mutable_integer`
+        let _mutable_integer = _mutable_integer;
+
+        // Error! `_mutable_integer` is frozen in this scope
+        // _mutable_integer = 50;
+        // FIXME ^ Comment out this line
+
+        // `_mutable_integer` goes out of scope
+    }
+
+    // Ok! `_mutable_integer` is not frozen in this scope
+    _mutable_integer = 3;
+
 }
